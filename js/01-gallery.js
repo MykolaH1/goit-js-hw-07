da3bda17b67a6d90b0ec76636a5d449341be6dea
MykolaH1 <mh22051993@gmail.com>
2022-08-10 21:14:32 +0300

    1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeGallery = ({ preview, original, description} = {}) => {
+   return `
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+   `; 
+};
+
+const galleryCardArr = galleryItems.map((el) => {
+   return makeGallery(el);
+});
+
+galleryEl.insertAdjacentHTML("beforeend", galleryCardArr.join(""));
+
+//-----------------------------------------------------------
+
+const onGalleryImgClick = event => {
+   event.preventDefault();
+
+   if(event.target.nodeName !== 'IMG') {
+      return;
+   }
+   
+
+
+
+
+
+   const bannerUrl = event.target.dataset.source;
+
+   const instance = basicLightbox.create(`
+      <img src="${bannerUrl}" width="800" height="600">
+   `)
+
+instance.show()
+}
+
+galleryEl.addEventListener('click', onGalleryImgClick);
+
+
+
