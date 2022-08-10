46cbab752fc7eb872653a5835a1e2d1bd54c4c1f
MykolaH1 <mh22051993@gmail.com>
2022-08-10 21:41:56 +0300

    2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeGallery = ({ preview, original, description } = {}) => {
+  return `
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+   `;
+};
+
+const galleryCardArr = galleryItems.map((el) => {
+  return makeGallery(el);
+});
+
+galleryEl.insertAdjacentHTML("beforeend", galleryCardArr.join(""));
+
+const onGalleryImgClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+};
+
+galleryEl.addEventListener("click", onGalleryImgClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  animationSpeed: 250,
+  captionsData: "alt",
+});
